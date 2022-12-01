9532d97d8e954bdd8a14dcf7129488ae0955abb0
Ursa Ursic <urska.ursic@gmail.com>
2022-12-01 11:27:31 +0100

    Initial commit

---
+++ utils.py
@@ -0,0 +1,23 @@
+"""
+Don't run this. 
+This is a utility file. 
+"""
+
+def sayHello():
+    print("Hello world!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("You ran the utility file. Not useful. Don't do it.")
+
